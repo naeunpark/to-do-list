50e45a0fcdb9cb8c299048842806afcca926ace3
naeunpark <paknaun@gmail.com>
2018-06-02 11:54:11 +1200

    splash page

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/es/integration/react';
+// import configureStore from './redux/configureStore';
+import LogIn from './components/LogIn/presenter';
 
-export default class App extends React.Component {
+const { persistor, store } = configureStore();
+
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
   render() {
+    const { isLoadingComplete } = this.state;
+
+    if(!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
+    
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <LogIn />
+        </PersistGate>
+      </Provider>
     );
   }
+
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/logo_trans.png")
+      ]),
+      Font.loadAsync([
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      ])
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.error(error);
+  };
+
+  _handleFinishLoading = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default App;
---
+++ redux/modules/user.js
@@ -0,0 +1,20 @@
+//Actions 
+
+//Action Creator  
+
+//initial State 
+const initialState = {
+    isLoggedIn: false
+};
+//reducer 
+function reducer(state = initialState, action) {
+switch (action.type) {
+    default:
+      return state;
+  }
+}
+//reducer action 
+
+//export
+
+export default reducer;
